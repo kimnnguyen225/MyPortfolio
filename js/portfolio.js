86d7d6d82f0ddb40ec99172a20d315f615a6d095
Kim Nguyen <kimnnguyen225@gmail.com>
2019-05-17 23:20:34 -0400

    add blog

--- js/portfolio.js
+++ js/portfolio.js
@@ -32,18 +32,18 @@ function setupEditor() {
     <span class="sign-color">&#62;</span>
     </p>
     <table style="font-size: 1.2rem; color: black border-collapse:separate; border-spacing:0 7px;">
+      <tr><td><i class="far fa-hand-point-right"></i> Contact: <a href="mailto: kimnnguyen225@gmail.com">
+                                                            kimnnguyen225@gmail.com</a> or 513-410-4388</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> Dedicated and dependable Software Developer having 2+ years of knowledge and experience in Waterfall and Agile methodologies. I possess expertise in Object-Oriented Analysis/Design, skilled at progressing from problem statements to well-documented designs with diverse operational experience in developing, testing, and supporting multiple languages, operating systems, and web applications.</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> I graduated from a full stack Java software development bootcamp with 2 days of Scrum Foundation from Cincy Code IT/MAX Technical Training located in Mason, OH. Hence, I'm currently PSM1 licensed. In addition, prior to Java bootcamp, I was also a Software Quality Assurance bootcamp graduate from Per Scholas, located in Cincinnati, OH.</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> In order to be a successful, well rounded Software Developer, I continue to grow my knowledge and <br> experience daily by learning significant skills from online resources.</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> Problem resolution, attentive, strong analytical, prioritizing, work under pressure, interpersonal, <br> and project management.</td></tr>
-      <tr><td><i class="far fa-hand-point-right"></i> Currently, I am seeking for a <strong>Junior Software Developer</strong> position in Cincinnati, OH and its <br>surrounding areas like Mason, Hamilton, BlueAsh, and West Chester.</td></tr>
       <tr><td><b class="word-color">Some of my favorite resources to geek:</b></td></tr>
-      <tr><td><i class="far fa-hand-point-right"></i> Youtube: <a href="#" onclick="window.open('https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA')">Traversy Media</a>, <a href="#" onclick="window.open('https://www.youtube.com/channel/UCJbPGzawDH1njbqV-D5HqKw')">The New Boston</a>, and more.</td></tr>
+      <tr><td><i class="far fa-hand-point-right"></i> Youtube: <a href="#" onclick="window.open('https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA')">Traversy Media</a>, <a href="#" onclick="window.open('https://www.youtube.com/channel/UCJbPGzawDH1njbqV-D5HqKw')">The New Boston</a>, <a href="#" onclick="window.open('https://www.youtube.com/channel/UC80PWRj_ZU8Zu0HSMNVwKWw/videos')">Codevolution</a>, and more.</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> stackoverflow</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> GitHub</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> W3Schools</td></tr>
       <tr><td><i class="far fa-hand-point-right"></i> Lynda</td></tr>
-      <tr><td><i class="far fa-hand-point-right"></i> freeCodeCamp</td></tr>
     </table>
   </body>
 </html>`,1); // 1 = moves cursor to end
